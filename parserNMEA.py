8340d7b0933676f2d53cdf16cecc68eefeaa8f98
Maciej Pławski <macplawski@gmail.com>
2022-01-13 17:22:53 +0100

    Czytanie pliku i wstepne parsowanie

--- parserNMEA.py
+++ parserNMEA.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import sys
+import re
+
+if __name__=="__main__":
+    print("blalba")
+    try:
+        file = open(sys.argv[1], "r")
+
+        for line in file:
+            x = re.split("\,", line[:-1])
+            print(x)
+    except e:
+        print(e)
