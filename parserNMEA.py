6178a3cf1fae485e15375395952651b0923018bf
Maciej Pławski <macplawski@gmail.com>
2022-01-14 12:44:36 +0100

    write to file parsed data

--- parserNMEA.py
+++ parserNMEA.py
@@ -1,14 +1,21 @@
 import sys
 import re
+from datetime import datetime
 
 if __name__=="__main__":
-    print("blalba")
+    data = []
     try:
-        file = open(sys.argv[1], "r")
-
-        for line in file:
+        fileToRead = open(sys.argv[1], "r")
+        fileToWrite = open("parseNMEA " + sys.argv[1], "w")
+        fileToWrite.write("Time Latitude N/S Longitude E/W qulity_indicator Satelites_used\n")
+        
+        for line in fileToRead:
             if re.search("GNGGA", line):
                 x = re.split("\,", line[:-1])
-                print(x)
+                fileToWrite.write(x[1]+" "+x[2]+" "+x[3]+" "+x[4]+" "+x[5]+" "+x[6]+" "+x[7]+"\n")
+                
+        fileToWrite.close()
+        fileToRead.close()
+        print(data)
     except e:
         print(e)
